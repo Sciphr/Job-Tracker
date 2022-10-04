a0a078b910f53a7e187de3f18364346a02646bbf
Sciphr <68548543+Sciphr@users.noreply.github.com>
2022-10-04 17:11:00 -0400

    testing changes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,7 +5,7 @@ import rateLimiter from 'express-rate-limit';
 
 const apiLimiter = rateLimiter({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 10,
+  max: 50,
   message: 'Too many requests from this IP address. Try again in 15 min',
 });
 const authRouter = express.Router();
